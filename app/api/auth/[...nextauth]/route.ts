925979808605173d64105b29922a3472f9bfbd8d
Leonardo <leonardo@imaige.com>
2023-11-29 16:39:26 -0400

    fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,4 @@
 import NextAuth, { AuthOptions } from "next-auth";
-export const runtime = 'edge';
-
 import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions as AuthOptions);
