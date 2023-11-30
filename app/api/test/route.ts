60abe76e291ca00aaa5d3e69ea3d7192464a60d1
Leonardo <leonardo@imaige.com>
2023-11-30 10:43:42 -0400

    test

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 export const runtime = 'edge';
 
-export function POST(request: Request) {
+export function GET(request: Request) {
   return new NextResponse(`I am a Serverless Function! ${process.env.GOOGLE_CLIENT_ID}`, {
     status: 200,
   });
