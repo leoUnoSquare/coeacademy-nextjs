389ef2f5557938ffdccfb7f8b542dc13198496e9
Leonardo <leonardo@imaige.com>
2023-11-30 14:53:37 -0400

    test geolocation api

--- components/organisms/AppHeader.tsx
+++ components/organisms/AppHeader.tsx
@@ -1,11 +1,18 @@
+import { geolocation } from '@vercel/edge';
+import { headers } from 'next/headers'
 import { AppBar, Toolbar, Container, Box, Typography } from "@mui/material";
 
 import GraphicEqIcon from "@mui/icons-material/GraphicEq";
 import { AddPlayListButton, AppSearch, UserDropdown } from "../molecules";
 
+export const runtime = 'edge';
+
 export default function AppHeader() {
+  const headerList = headers();
+  const {city} = geolocation({headers: headerList})
   return (
     <AppBar position="fixed">
+      {`you are in `+ city}
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <GraphicEqIcon sx={{ mr: 2 }} />
